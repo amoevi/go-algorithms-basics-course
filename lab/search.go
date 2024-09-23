bb37582716525d66dfbe5c6b285660d261fe39ca
Alexandre Moevi <alexandre.moevi@protonmail.com>
2024-09-23 12:23:02 +0200

    search exercises

--- lab/search.go
+++ lab/search.go
@@ -1,1 +1,9 @@
 package main
+
+// TODO
+// Create the function linearSearch that takes a slice of integers and a target value
+// and returns the index of the target if found, or -1 if not found.
+
+// TODO
+// Create the function binarySearch that takes a sorted slice and a target value
+// and returns the index of the target or -1 if not found.
