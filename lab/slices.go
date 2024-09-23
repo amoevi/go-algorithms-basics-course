8b4fae7c155f822408c1e7834bc7165d5e17c7f3
Alexandre Moevi <alexandre.moevi@protonmail.com>
2024-09-23 11:20:31 +0200

    starting lab

---
+++ lab/slices.go
@@ -0,0 +1,16 @@
+package main
+
+// TODO
+// Create the function generateSequentialSlice that takes an integer n
+// and returns a slice containing numbers from 0 to n-1
+
+// TODO
+// Create the function generateIdenticalSlice that takes an integer n and an integer value
+// and returns a slice of n integers where every element is equal to the given value
+
+// TODO
+// Create the function generateRandomSlice that takes an integer n and an integer max
+// and returns a slice of n random integers.
+//
+// Note: rand.Intn(max) generates a random number between 0 and max-1.
+// For example, rand.Intn(100) will generate a number between 0 and 99.
